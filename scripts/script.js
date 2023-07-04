685b80f682e574a48f0d1ec3d8787464cb23e475
Luan <luam10@hotmail.com.br>
2023-07-04 10:15:06 -0300

    git init

---
+++ scripts/script.js
@@ -0,0 +1,100 @@
+const FRONT = "card_front"
+const BACK = "card_back"
+const CARD = "card"
+const ICON = "icon"
+
+function newGame(){
+    stardGame()
+    let home = document.getElementById("home")
+    home.style.display = 'none'
+}
+
+function stardGame() {
+    initializeCards(game.createCardsFromTechs())
+}
+
+function initializeCards(cards) {
+    let gameBoard = document.getElementById("gameBoard")
+    gameBoard.innerHTML = ''
+    game.cards.forEach(card => {
+        let cardElement = document.createElement('div')
+        cardElement.id = card.id
+        cardElement.classList.add(CARD)
+        cardElement.dataset.icon = card.icon
+
+        createCardContent(card, cardElement)
+
+        cardElement.addEventListener('click', flipCard)
+        gameBoard.appendChild(cardElement)
+    })
+
+}
+
+function createCardContent(card, cardElement) {
+    createCardFace(FRONT, card, cardElement)
+    createCardFace(BACK, card, cardElement)
+}
+
+function createCardFace(face, card, element) {
+    let cardElementFace = document.createElement('div')
+    cardElementFace.classList.add(face)
+    if (face === FRONT) {
+        let iconElement = document.createElement('img')
+        iconElement.classList.add(ICON)
+        iconElement.src = "./images/" + card.icon + ".png"
+        cardElementFace.appendChild(iconElement)
+    } else {
+        cardElementFace.innerHTML = "&lt/&gt"
+    }
+    element.appendChild(cardElementFace)
+}
+
+function flipCard() {
+    if (game.setCard(this.id)) {
+        this.classList.add("flip")
+        if (game.secondCard) {
+            let numberGame = document.getElementById("number")
+            let counter = document.getElementById("counter")
+
+            var total = numberGame.innerHTML ++
+            counter.style.background = 'red'
+            setTimeout(()=>{
+                counter.style.background = ''
+            },400)
+
+            if (game.checkMatch()) {
+                game.clearCards()
+                counter.style.background = 'green'
+                setTimeout(()=>{
+                    counter.style.background = ''
+                },400)
+                if(game.checkGameOver()){
+                    let gameOverLayer = document.getElementById("gameOver")
+                    gameOverLayer.style.display = 'flex'
+                    let score = document.getElementById("totalMoves")
+                    score.innerHTML = Number(score.innerHTML)+ total
+                    console.log(score) 
+
+                    numberGame.innerHTML = 0 
+
+                }
+            } else {
+                setTimeout(() => {
+                    let firstCardView = document.getElementById(game.firstCard.id)
+                    let secondCardView = document.getElementById(game.secondCard.id)
+
+                    firstCardView.classList.remove('flip')
+                    secondCardView.classList.remove('flip')
+                    game.unflipCards()
+                }, 1000)
+            }
+        }
+    }
+
+}
+
+function restart(){
+    stardGame()
+    let gameOverLayer = document.getElementById("gameOver")
+    gameOverLayer.style.display = 'none'
+}
